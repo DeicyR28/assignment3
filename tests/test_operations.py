3339a9589165a73e634f9ee52a85f345f22de6ef
DeicyR28 <deicyrod28@gmail.com>
2026-02-07 16:06:02 -0500

    clone of assignment 2 added parametrize testing for addition test method

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,8 +1,32 @@
 import pytest   
 from app.operations import addition, division, multiplication, substraction
+from typing import Union
 
-def test_addition():
-    assert addition(1,1) == 2
+Number = Union[int, float]
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [ 
+        (2, 2, 4),
+        (0, 0, 0),
+        (-1, 1, 0 ),
+        (2.5, 3.5, 6.0),
+        (-2.5, 3.5, 1.0),
+    ],
+    ids= [ 
+        "add_two_positive_integers",
+        "add_two_zeros",
+        "add_negative_and_positive_integer",
+        "add_two_positive_floats",
+        "add_negative_float_and_positive_float",
+    ]
+)
+def test_addition(a: Number, b: Number, expected: Number) -> None:
+
+    result = addition(a,b)
+
+    assert result == expected, f"Expected addition({a}, {b}) to be expected{expected}, but got {result}" 
+
   
 def test_substraction():
     assert substraction(1,1) == 0
